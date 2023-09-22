e8cc7c4a66f9d6a02d29f1766533c9ac2dfbccee
= <eilersd23@gmail.com>
2023-09-22 10:19:36 -0400

    Created basic routes for authentication

--- server/app/auth/views.py
+++ server/app/auth/views.py
@@ -1,7 +1,20 @@
 from flask import request
-from . import app
+from flask_login import login_user, logout_user, login_required, current_user
+from . import auth
 
-@app.route("/login", methods=['POST'])
+@auth.route("/login", methods=['POST'])
 def login():    
     email = request.form.get("email")
     password = request.form.get("password")
+
+@auth.route("/register", methods=['POST'])
+def register():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password1")
+    confirm_password = request.form.get("password2")
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
